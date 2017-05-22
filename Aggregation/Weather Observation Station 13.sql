0ac4a2bb69cd723f2fa5865179e934f0072bec4c
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 10:22:37 -0700

    Create Weather Observation Station 13.sql

---
+++ Aggregation/Weather Observation Station 13.sql
@@ -0,0 +1,6 @@
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/weather-observation-station-13
+*/
+
+SELECT ROUND(SUM(LAT_N),4) FROM station WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
