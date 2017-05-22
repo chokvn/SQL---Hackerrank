044629f0c1c4d8e9ff2e40d975a33753dd47ba1c
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 09:55:52 -0700

    Update Weather Observation Station 05.sql

--- Basic Select/Weather Observation Station 05.sql
+++ Basic Select/Weather Observation Station 05.sql
@@ -1,3 +1,10 @@
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/weather-observation-station-5
+
+This can also be done with 2 select statements without union.
+*/
+
 (SELECT CITY, length(CITY) FROM STATION ORDER BY (LENGTH(CITY)) ASC, CITY ASC LIMIT 1)
 UNION
 (SELECT city, LENGTH(CITY) from station order by (length(city)) DESC, city ASC LIMIT 1);
