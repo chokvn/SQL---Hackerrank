d861acf763e926504dda456d2895b5233ec6d101
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 09:54:46 -0700

    Update Weather Observation Station 06.sql

--- Basic Select/Weather Observation Station 06.sql
+++ Basic Select/Weather Observation Station 06.sql
@@ -1,5 +1,11 @@
-/* Would prefer to use
+/* 
+Challenge reference:
+https://www.hackerrank.com/challenges/weather-observation-station-6
+
+Would prefer to use
       SELECT DISTINCT city FROM station WHERE UPPER(city) RLIKE '^[AEIOU].*$';
-   But the challenge does not accept this. In normal queries, would also ensure city LIKE ... has UPPER(city) */
+But the challenge does not accept this. In normal queries, would also ensure city LIKE ... has UPPER(city).
+*/
+
 SELECT DISTINCT city FROM station WHERE city LIKE 'A%' or city LIKE 'E%' or city LIKE 'I%' or city like 'O%' or city like 'U%'
   ORDER BY city;
