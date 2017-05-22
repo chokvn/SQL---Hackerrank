90efdc3e980ee4c632f35e56ee81cf2c87979eed
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 09:53:35 -0700

    Update Weather Observation Station 08.sql

--- Basic Select/Weather Observation Station 08.sql
+++ Basic Select/Weather Observation Station 08.sql
@@ -1,3 +1,8 @@
-/* Contrary to previous two exercises, RLIKE functions just fine here.
-   UPPER(city) used to avoid [AEIOUaeiou] in RLIKE condition. */
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/weather-observation-station-8
+
+Contrary to previous two exercises, RLIKE functions just fine here.
+UPPER(city) used to avoid [AEIOUaeiou] in RLIKE condition.
+*/
 SELECT DISTINCT city FROM station WHERE UPPER(city) RLIKE '^[AEIOU].*[AEIOU]$';
