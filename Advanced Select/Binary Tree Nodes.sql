4959f5f095090c0ef085d63e717fb03ac4509247
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 10:11:43 -0700

    Create Binary Tree Nodes.sql

---
+++ Advanced Select/Binary Tree Nodes.sql
@@ -0,0 +1,11 @@
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/binary-search-tree-1
+*/
+
+SELECT N, (CASE
+    WHEN P IS NULL THEN 'Root'
+    WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+    ELSE 'Leaf'
+    END)
+FROM BST ORDER BY N;
