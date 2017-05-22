df33ea506e3d16bfb0c968dc97a58b3d2f916d0a
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 09:54:06 -0700

    Update Weather Observation Station 07.sql

--- Basic Select/Weather Observation Station 07.sql
+++ Basic Select/Weather Observation Station 07.sql
@@ -1,4 +1,10 @@
-/* Similar to weather observation station 7, skipping over RLIKE */
+/* 
+Challenge reference:
+https://www.hackerrank.com/challenges/weather-observation-station-7
+
+Similar to weather observation station 7, skipping over RLIKE
+*/
+
 SELECT DISTINCT city FROM station
 WHERE city LIKE '%a'or city LIKE '%e' or city LIKE '%i' or city LIKE '%o' or city LIKE '%u'
 ORDER BY city;
