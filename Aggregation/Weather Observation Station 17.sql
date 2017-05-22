340d404bf2fe5712e42dd8e28c53d06e7402fe79
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 10:25:58 -0700

    Create Weather Observation Station 17.sql

---
+++ Aggregation/Weather Observation Station 17.sql
@@ -0,0 +1,6 @@
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/weather-observation-station-17
+*/
+
+SELECT ROUND(LONG_W, 4) FROM station WHERE LAT_N > 38.7780 ORDER BY LAT_N ASC LIMIT 1;
