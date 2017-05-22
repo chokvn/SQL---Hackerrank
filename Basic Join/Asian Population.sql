f02e78e1db06aae05d8709c47a18d863cce2d2c0
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 10:28:31 -0700

    Create Asian Population.sql

---
+++ Basic Join/Asian Population.sql
@@ -0,0 +1,6 @@
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/asian-population
+*/
+
+SELECT SUM(city.population) FROM city INNER JOIN country ON city.countrycode = country.code WHERE country.continent = 'Asia';
