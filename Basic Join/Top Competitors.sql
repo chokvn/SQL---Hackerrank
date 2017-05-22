4065fc93c493bdcc4188a832c5383382feef2c62
Kevin Cho, Ph.D <cho.kvn@gmail.com>
2017-05-22 14:45:24 -0700

    Create Top Competitors.sql

---
+++ Basic Join/Top Competitors.sql
@@ -0,0 +1,14 @@
+/*
+Challenge reference:
+https://www.hackerrank.com/challenges/full-score
+
+Ensure coding is on MySQL!
+*/
+
+SELECT h.hacker_id, h.name FROM submissions s	
+  INNER JOIN challenges c ON s.challenge_id = c.challenge_id
+  INNER JOIN difficulty d ON c.difficulty_level = d.difficulty_level
+  INNER JOIN hackers h ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score
+GROUP BY h.hacker_id, h.name HAVING COUNT(s.hacker_id) > 1
+ORDER BY count(s.hacker_id) DESC, s.hacker_ID ASC;
